92ef71cf03be65723205cbae1878b3133cb1c975
slipkorn20 <72992285+slipkorn20@users.noreply.github.com>
2022-02-26 08:35:17 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// დაპოსტვა/შექმნა CREATE/POST 
+// რედაქტირება UPDATE PUT/PATCH
+// წაშლა DELETE
+// წამოღება GET
+
+
+// ბლოგების სია  - > POSTS 
+// Endpoints :
+// getPosts : GET
+// createPost: POST
+// deletePost:  DELETE
+// updatePost : PUT/PATCH
+
+
+const database = [
+    {
+        title: 'Pirveli blogi',
+        text: 'Lorem ipsum',
+        Thumbnail: '',
+        author: 'oto',
+
+    }
+    {
+        title: 'meore blogi',
+        text: 'Lorem ipsum',
+        Thumbnail: '',
+        author: 'oto',
+
+    }
+    {
+        title: 'mesame blogi',
+        text: 'Lorem ipsum',
+        Thumbnail: '',
+        author: 'oto',
+
+    }
+    {
+        title: 'meotxe blogi',
+        text: 'Lorem ipsum',
+        Thumbnail: '',
+        author: 'oto',
+
+    }
+
+]
